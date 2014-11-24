5393c4c69327ed15b4491eea3d2b90644a81992b
billy <billy-hoang@hotmail.com>
2014-11-23 23:38:56 -0800

    Added banner, search bar, more information per album

--- app/views/customer.blade.php
+++ app/views/customer.blade.php
@@ -1,13 +1,29 @@
 @extends('partials.master');
 
 @section('content')
+<div class="jumbotron text-center" style="background-color: black">
+	<div class="container">
+		<h1 style="color: #FFFFFF">Explore!</h1>
+		<p style="color: #C0C0C0">Find your favorite songs here</p>
+	</div>
+</div>
+<label>Search By: </label><br>
+Category: <input ng-model="category" name="category" id="category" placeholder="Rock">
+Title: <input ng-model="title" name="title" id="title" placeholder="Album Title">
+Artitst: <input ng-model="artist" name="artiest" id="artist" placeholder="Justin Bieber">
+Quantity: <input ng-model="quantity" name="quantity" id="quantity" placeholder="50"> &nbsp;
+<button id="search" class="btn btn-primary" ng-click="search()">Search</button>
+<br><hr>
+
 
 <section id="demo" style="position: relative; width: 100%">
  
  	@foreach($albums as $album)
 	<article class="white-panel" style="position: absolute; border-style: solid; border-width: 1px; padding: 10px">
-		<h1><a href="#">Album: {{ $album->title }}</a></h1>
+		<h1><a href="#">Album: {{ $album->title }}</a></h1><hr>
 		<h2>Artist: {{ $album->leadSinger }}</h2>
+		<h4>In Stock: {{$album->stock}}</h4>
+		<h5>Category: {{$album->category}}</h5><hr>
 		@foreach($album->songs as $song)
 			<p>Song: {{ $song->title }}</p>
 		@endforeach
