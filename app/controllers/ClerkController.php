f31a69e68debb6c58674bf74d461a80e7dc00e53
kye-13 <kye.13@hotmail.com>
2014-11-24 11:38:23 -0800

    added functions to clerk controller

--- app/controllers/ClerkController.php
+++ app/controllers/ClerkController.php
@@ -22,7 +22,66 @@ class ClerkController extends BaseController {
 
 	public function checkAlbumDate() 
 	{
+		$timestamp = date('Y-m-d');
+
 		
+		
+	}
+
+	//verify the receiptId
+	public function verifyReceipt()
+	{
+		DB::transaction(function(){
+			$query = DB::table('Order')
+							->where('receiptId', '=', 'input from user')
+							-> get();
+		});
+	}
+
+	//check if receipt is in Refund table
+	public function alreadyReturned()
+	{
+		Db::transaction(function(){
+			$query = DB::table('Refund')
+							->where('receiptId', '=', 'input from user')
+							->get();
+		});
+	}
+
+	// process refund
+	public function processRefund()
+	{
+		$timestamp = date('y-m-d');
+
+			DB::transaction(function(){
+				DB::table('Refund')->insert(
+					array(
+						'retid' => 'auto generated id',
+						'date' => $timestamp,
+						'receiptId' => 'original receipt id'
+						)
+					);
+			});
+	}
+
+	// update the stock
+	public function updateStockForRefund()
+	{
+		DB::transaction(function(){
+			$receiptID = DB::table('PurchaseItem')->where(
+			'receiptID', 'user input receiptID')
+			-> first();
+
+			$qty = var_dump($stock->qty);
+			$upc = var_dump($stock->upc);
+
+			DB::table('Item')
+					->where('upc', $upc)
+					->update(array(
+						'stock' => 'stock' + $qty)
+					);
+		});
+
 	}
 
 }
