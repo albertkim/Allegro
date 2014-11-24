b93a08aa8389131aa987e6f47e57fd3ca9f0d0af
Albert <albert275@gmail.com>
2014-11-23 19:38:00 -0800

    Better customer view

--- app/controllers/CustomerController.php
+++ app/controllers/CustomerController.php
@@ -16,8 +16,24 @@ class CustomerController extends BaseController {
 	*/
 
 	public function get(){
-		log::info("Current user: " + Cookie::get("user"));
-		return View::make("customer");
+		log::info("Current user: ");
+		log::info( Cookie::get("user"));
+		$albums = DB::table("item")->get();
+
+		// get all related data to albums, add to album objects
+		foreach($albums as $album){
+			$id = intval($album->upc);
+			$leadSinger = DB::table("leadSinger")->where("upc", $id)->first();
+			$songs = DB::table("hasSong")->where("upc", $id)->get();
+
+			// if exists
+			$album->leadSinger = $leadSinger->name;
+			$album->songs = $songs;
+		}
+		
+		return View::make("customer", array(
+			"albums" => $albums,
+		));
 	}
 
 }
